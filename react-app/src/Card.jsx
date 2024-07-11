6fed28ce16f15397943d1cafcde812ca4f32636b
= <=>
2024-07-12 07:56:13 +0800

    added css to the card

--- react-app/src/Card.jsx
+++ react-app/src/Card.jsx
@@ -4,7 +4,7 @@ function Card(){
    
     return(
         <div className="card">
-            <img src={profilePicture} alt="profile picture"></img>
+            <img className="card-image" src={profilePicture} alt="profile picture"></img>
             <h2>Dev Jas</h2>
             <p>I build sites for a living</p>
         </div>
