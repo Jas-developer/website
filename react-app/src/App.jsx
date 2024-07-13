1f96ddc4e4fa418fbf532476b70ceda22b8c9a69
= <=>
2024-07-14 06:55:50 +0800

    good morning

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,16 @@
 import Button from "./Button";
+import Student from "./Student"
+
+
 
 function App() {
   return(
     <>
+    <Student name="Spongebob" age={23} isStudent={true} school="SHNHS"/>
+    <Student name="Patrick" age={23} isStudent={false} school="SHNHS"/>
+    <Student name="Squidward" age={23} isStudent={false} school="SHNHS"/>
+    <Student name="Sandy" age={23} isStudent={true} school="SHNHS"/>
+    {/* button */}
     <Button/>
     </>
   );
---
+++ react-app/src/Student.jsx
@@ -0,0 +1,24 @@
+
+import propTypes from "prop-types";
+
+function Student(props){
+    return(
+        <div className="student">
+            <p>Name: {props.name}</p>
+            <p>School: {props.school}</p>
+            <p>Age: {props.age}</p>
+            <p>Status: {props.isStudent ? <p>{props.name} is a Student</p> : "That boy is not a student"}</p>
+        </div>
+    )
+};
+
+
+
+Student.propTypes = {
+    name:propTypes.string,
+    age:propTypes.number,
+    school:propTypes.string,
+    isStudent: propTypes.bool
+}
+
+export default Student;
