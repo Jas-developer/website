ee9b408f5ac8e408bb6990aae2525281db20890f
= <=>
2024-08-01 19:44:32 -0400

    learning list

--- react-app/src/List.jsx
+++ react-app/src/List.jsx
@@ -1,17 +1,8 @@
-function List(){
+function List(props){
 
-    
-
-        // Sorting an array objects
-        fruits.sort((a,b) => a.name.localeCompare(b.name)); //ALPHABETICAL
-        fruits.sort((a,b) => b.name.localeCompare(a.name));// REVERSE ALPHABETICAL
-        fruits.sort((a,b) => a.calories - b.calories); //NUMERIC
-        fruits.sort((a,b) => b.calories - a.calories); // REVERSE NUMERIC
-    
-    const listItems = fruits.map(fruit => <li key={fruit.name}>{fruit.name}: &nbsp; {fruit.calories};</li>)
-    
-
-    return ( <ol>{listItems}</ol>);
+const itemList  = props.items;  
+    const listItems = itemList.map(fruit => <li key={fruit.name}>{fruit.name}: &nbsp; {fruit.calories};</li>)   
+    return ( <><h3>{listItems.calories}</h3><ol>{listItems}</ol></>);
 };
 
 export default List;
