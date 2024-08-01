24327bb5095ed9f3219e4d388d1c2dae516b8c27
= <=>
2024-08-01 19:34:54 -0400

    handling object arrays

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,9 +3,17 @@ import List from "./List";
 
 
 function App() {
+
+  const fruits = [
+        {name:"apple", calories: 47},
+        {name:"orange", calories:96},
+        {name:"banana", calories: 45},
+        {name:"coconut", calories:159}, 
+        {name:"pineapple", calories: 37}];
+
   return(
  <div>
-  <List/>
+  <List items={fruits} category="Fruits"/>
  </div>
   );
 
--- react-app/src/List.jsx
+++ react-app/src/List.jsx
@@ -1,8 +1,14 @@
 function List(){
 
-    const fruits = ["apple","orange","banana","coconut", "pineapple"];
     
-    const listItems = fruits.map(fruit => <li>{fruit}</li>)
+
+        // Sorting an array objects
+        fruits.sort((a,b) => a.name.localeCompare(b.name)); //ALPHABETICAL
+        fruits.sort((a,b) => b.name.localeCompare(a.name));// REVERSE ALPHABETICAL
+        fruits.sort((a,b) => a.calories - b.calories); //NUMERIC
+        fruits.sort((a,b) => b.calories - a.calories); // REVERSE NUMERIC
+    
+    const listItems = fruits.map(fruit => <li key={fruit.name}>{fruit.name}: &nbsp; {fruit.calories};</li>)
     
 
     return ( <ol>{listItems}</ol>);
