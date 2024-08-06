27a495f65b1e7231cba54697437cf92c0b61b436
= <=>
2024-08-06 19:17:29 -0400

    conditional rendering

--- react-app/src/List.jsx
+++ react-app/src/List.jsx
@@ -1,8 +1,17 @@
-function List(props){
-
-const itemList  = props.items;  
-    const listItems = itemList.map(fruit => <li key={fruit.name}>{fruit.name}: &nbsp; {fruit.calories};</li>)   
-    return ( <><h3>{listItems.calories}</h3><ol>{listItems}</ol></>);
-};
+function List(props) {
+  const itemList = props.items;
+  const categories = props.category;
+  const listItems = itemList.map((fruit) => (
+    <li key={fruit.name}>
+      {fruit.name}: &nbsp; {fruit.calories};
+    </li>
+  ));
+  return (
+    <>
+      <h3 className="list-category">{categories}</h3>
+      <ol className="list-items">{listItems}</ol>
+    </>
+  );
+}
 
 export default List;
