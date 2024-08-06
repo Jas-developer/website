6fc3d8a53498b6446c2e11e829589ef1faf0449b
= <=>
2024-08-06 19:57:07 -0400

    learning basics

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,32 +1,11 @@
-import List from "./List";
-
 function App() {
-  const fruits = [
-    { id: 1, name: "apple", calories: 47 },
-    { id: 2, name: "orange", calories: 96 },
-    { id: 3, name: "banana", calories: 45 },
-    { id: 4, name: "coconut", calories: 159 },
-    { id: 5, name: "pineapple", calories: 37 },
-  ];
-
-  const vegetables = [
-    {
-      id: 6,
-      name: "Potatoes",
-      calories: 110,
-    },
-    { id: 7, name: "celery", calories: 15 },
-    { id: 8, name: "carrots", calories: 25 },
-    { id: 9, name: "corn", calories: 200 },
-    { id: 10, name: "brocoli", calories: 50 },
-  ];
+  const work = (param) => {
+    return { toString: () => param }; // converting an ovbject property into string value
+  };
 
   return (
     <>
-      {fruits.length > 0 ? <List items={fruits} category="Fruits" /> : null}
-      {vegetables.length > 0 ? (
-        <List items={vegetables} category="Vegetables" />
-      ) : null}
+      <h1>Hi. I am working as a {work("Software Engineer!").toString()}</h1>
     </>
   );
 }
