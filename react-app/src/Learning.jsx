8d62701e280f6e3fe888ffe603cc05109f642a36
= <=>
2024-08-07 19:23:29 -0400

    react destructuring

--- react-app/src/Learning.jsx
+++ react-app/src/Learning.jsx
@@ -1,6 +1,19 @@
 export default function Learning() {
   const fruits = [{ name: "apple" }, { name: "banana" }, { name: "orage" }];
 
+  // learning destructioring in react/js
+
+  const calculate = (a, b) => {
+    const add = a + b;
+    const subtract = a - b;
+    const multiply = a * b;
+    const divide = a / b;
+
+    return [add, subtract, multiply, divide];
+  };
+
+  const [add, subtract, multiply, divide] = calculate(2, 3);
+
   return (
     <div>
       {fruits.map((fruit, index) => (
