868a1fda204cbbc91257673c1ee5cd3834118aff
= <=>
2024-08-12 19:34:20 -0400

    learned how to render list in react

--- react-app/src/List.jsx
+++ react-app/src/List.jsx
@@ -1,17 +1,23 @@
-function List(props) {
-  const itemList = props.items;
-  const categories = props.category;
-  const listItems = itemList.map((fruit) => (
-    <li key={fruit.name}>
-      {fruit.name}: &nbsp; {fruit.calories};
-    </li>
-  ));
+// In react you will render list with some type of loop
+function Car(props) {
+  return <l1>I am a {props.brand}</l1>;
+}
+
+function Garage() {
+  const cars = [
+    { id: 1, brand: "Ford" },
+    { id: 2, brand: "BMW" },
+    { id: 3, brand: "AUDI" },
+  ];
+
   return (
     <>
-      <h3 className="list-category">{categories}</h3>
-      <ol className="list-items">{listItems}</ol>
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car) => (
+          <Car key={car.id} brand={car.brand} />
+        ))}
+      </ul>
     </>
   );
 }
-
-export default List;
