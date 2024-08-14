44cde83c0ae89375a19b7547c512f602af8e9196
= <=>
2024-08-14 19:11:41 -0400

    Learning form

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import Learning from "./Learning";
+import MyForm from "./Forms";
 function App() {
   const work = (param) => {
     return { toString: () => param }; // converting an ovbject property into string value
@@ -8,6 +9,9 @@ function App() {
     <>
       <h1>Hi. I am working as a {work("!").toString()}</h1>
       <h2>This is the fruits {<Learning />}</h2>
+      <br />
+      <h2>This is my form</h2>
+      <MyForm />
     </>
   );
 }
