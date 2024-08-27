458d5292d42016286137defc73eb033062f6d9a0
= <=>
2024-08-27 19:19:33 -0400

    got back to react world

--- mini-context/src/App.jsx
+++ mini-context/src/App.jsx
@@ -1,9 +1,13 @@
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
   return (
     <UserContextProvider>
       <h1>React for context api!</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
   );
 }
