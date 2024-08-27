f1b1416a9883b9989d6222e6c70570c9291985f6
= <=>
2024-08-27 19:19:39 -0400

    wqeqwe

--- mini-context/src/components/Profile.jsx
+++ mini-context/src/components/Profile.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 function Profile(props) {
-  return <div>Profile: {props.username}</div>;
+  return (
+    <div>
+      Profile: {}
+      <h1>More components</h1>
+    </div>
+  );
 }
+
+export default Profile;
